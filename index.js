fd271e808277013d90e2266f7ab8b635f00663ba
+ <+>
2023-03-15 17:16:33 -0400

    Welcome, M

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+let currentUser = "Mike"
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"; 
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let firstInitial = "M"
+let shortGreeting = "Welcome, " + firstInitial + "!";
+
+
+
+
+
+
